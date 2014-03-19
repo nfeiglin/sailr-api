91823cf6ad1ad26604f154df5760428102e3b5ec
Nathan Feiglin <nathan@technewsu.com.au>
2014-03-19 20:59:24 +1100

    Adds ProfileImgs table (migrations, model, seeder) Add Model relationship for user<>profileimg Adds Intervention Image class to Composer, and config.php providers + Alias

--- seeds/ItemsTableSeeder.php
+++ seeds/ItemsTableSeeder.php
@@ -8,8 +8,9 @@ class ItemsTableSeeder extends Seeder {
 		// Uncomment the below to wipe the table clean before populating
 		// DB::table('items')->truncate();
         $faker = Faker::create();
-        foreach(range(1, 10) as $index) {
-            Item::create([
+        $items = Item::all();
+        foreach($items as $index) {
+          /*  Item::create([
                 'user_id' => $index,
                 'description' => 'An incredible, amazing, cool, life-changing description goes here! ' . $index,
                 'title' => 'A life-changing title goes here!',
@@ -17,6 +18,11 @@ class ItemsTableSeeder extends Seeder {
                 'initial_units' => $index
 
             ]);
+
+          */
+
+            $index->price = $faker->randomFloat(2, 2.00, 200.00);
+            $index->save();
         }
 
 		// Uncomment the below to run the seeder
