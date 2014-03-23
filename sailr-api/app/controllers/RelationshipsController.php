109f0966f86a263af7a98326b3584c36f9b29c88
Nathan Feiglin <nathan@technewsu.com.au>
2014-03-23 19:37:51 +1100

    Adds ability to follow, unfollow and view follow status.

--- sailr-api/app/controllers/RelationshipsController.php
+++ sailr-api/app/controllers/RelationshipsController.php
@@ -4,55 +4,146 @@ class RelationshipsController extends \BaseController
 {
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @return Response
+     * @return Response::json
      */
     public function store()
     {
-        //
+        $input = Input::all();
+        $user = Auth::user();
+        if (array_key_exists('username', $input)) {
+            $followUser = User::where('username', '=', $input['username'])->firstOrFail(array('id', 'name', 'username'));
+        } else if (array_key_exists('user_id', $input)) {
+            $followUser = User::where('id', '=', $input['user_id'])->firstOrFail(array('id', 'name', 'username'));
+        } else {
+            $res = array(
+                'meta' => array(
+                    'statuscode' => 400,
+                    'message' => 'Correct identification for user to follow was not provided'
+                )
+            );
+
+            return Response::json($res, 400);
+        }
+
+        $doesExist = DB::table('relationships')->where('user_id', '=', $user->id)->where('follows_user_id', '=', $followUser->id)->count();
+
+        if ($doesExist | $followUser->id == $user->id) {
+            $res = array(
+                'meta' => array(
+                    'statuscode' => 403,
+                    'message' => 'You already follow ' . $followUser->username
+                )
+            );
+            return Response::json($res, 403);
+        }
+
+        $relationship = new Relationship();
+        $relationship->user_id = $user->id;
+        $relationship->follows_user_id = $followUser->id;
+
+        $relationship->save();
+
+        $res = array(
+            'meta' => array(
+                'statuscode' => 201,
+                'message' => 'Successfully followed ' . $followUser->username
+            )
+        );
+
+        return Response::json($res, 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     *
+     * @return Response
+     */
+    public function destroy()
+    {
+        /*
+         * Example POST DATA:
+         * {
+         *      "username": "MZ"
+         * }
+         */
+        $input = Input::all();
+        $user = Auth::user();
+
+        if (array_key_exists('username', $input)) {
+            $followUser = User::where('username', '=', $input['username'])->firstOrFail(array('id', 'name', 'username'));
+        } else if (array_key_exists('user_id', $input)) {
+            $followUser = User::where('id', '=', $input['user_id'])->firstOrFail(array('id', 'name', 'username'));
+        } else {
+            $res = array(
+                'meta' => array(
+                    'statuscode' => 400,
+                    'message' => 'Correct identification for user to unfollow was not provided'
+                )
+            );
+
+            return Response::json($res, 400);
+        }
+
+        $doesExist = DB::table('relationships')->where('user_id', '=', $user->id)->where('follows_user_id', '=', $followUser->id)->count();
+
+        if (!$doesExist) {
+            $res = array(
+                'meta' => array(
+                    'statuscode' => 403,
+                    'message' => "You don't follow" . $followUser->username . " so you can't unfollow them"
+                )
+            );
+            return Response::json($res, 403);
+        }
+
+        $relationship = Relationship::where('user_id', '=', $user->id)->where('follows_user_id', '=', $followUser->id);
+        $relationship->delete();
+        $res = array(
+            'meta' => array(
+                'statuscode' => 200,
+                'message' => 'Successfully unfollowed ' . $followUser->username
+            )
+        );
+
+        return Response::json($res, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int $id
      * @return Response
      */
-    public function show($id)
+    public function show()
     {
-        //
-    }
+        $input = Input::all();
+        $user = Auth::user();
+        if (array_key_exists('username', $input)) {
+            $checkUser = User::where('username', '=', $input['username'])->firstOrFail(array('id', 'name', 'username'));
+        } else if (array_key_exists('user_id', $input)) {
+            $checkUser = User::where('id', '=', $input['user_id'])->firstOrFail(array('id', 'name', 'username'));
+        } else {
+            $res = array(
+                'meta' => array(
+                    'statuscode' => 400,
+                    'message' => 'Correct identification for relationship to show was not provided'
+                )
+            );
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
+            return Response::json($res, 400);
+        }
+
+        $res = array(
+            'meta' => array(
+                'statuscode' => 200,
+            )
+        );
+        $res['follows_you'] = RelationshipHelpers::follows_you($checkUser);
+        $res['you_follow'] = RelationshipHelpers::you_follow($checkUser);
+
+        return Response::json($res, 200);
     }
 
     /**
@@ -66,15 +157,5 @@ class RelationshipsController extends \BaseController
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 
 }
---
+++ sailr-api/app/helpers/RelationshipHelpers.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * User: Nathan
+ * Date: 23/03/14
+ * Time: 4:00 PM
+ */
+
+
+class RelationshipHelpers
+{
+    public static function follows_you(User $user)
+    {
+        $doesExist = DB::table('relationships')->where('user_id', '=', $user->id)->where('follows_user_id', '=', Auth::user()->id)->count();
+        if ($doesExist) {
+            return true;
+        }
+        return false;
+    }
+
+    public static function you_follow(User $user)
+    {
+        $doesExist = DB::table('relationships')->where('user_id', '=', Auth::user()->id)->where('follows_user_id', '=', $user->id)->count();
+        if ($doesExist) {
+            return true;
+        }
+        return false;
+    }
+
+    public static function get_follows_user(User $user)
+    {
+        $followers = Relationship::where('follows_user_id', '=', $user->id)->orderBy('created_at', 'dsc')->get(array('user_id'))->toArray();
+
+        $arrayOne = array();
+        $counter = 0;
+        foreach ($followers as $key => $value) {
+            $arrayOne[$counter] = $value['user_id'];
+            $counter = $counter + 1;
+        }
+
+        $users = User::whereIn('id', $arrayOne)->with(array(
+            'ProfileImg' => function ($y) {
+                    $y->select(['id', 'type', 'url']);
+                }
+        ))->get()->toArray();
+
+        return $users;
+    }
+
+    public static function get_user_following(User $user)
+    {
+        $following = Relationship::where('user_id', '=', $user->id)->orderBy('created_at', 'dsc')->get(array('follows_user_id'))->toArray();
+
+        $arrayOne = array();
+        $counter = 0;
+        foreach ($following as $key => $value) {
+            $arrayOne[$counter] = $value['follows_user_id'];
+            $counter = $counter + 1;
+        }
+
+        $users = User::whereIn('id', $arrayOne)->with(array(
+            'ProfileImg' => function ($y) {
+                    $y->select(['id', 'type', 'url']);
+                }
+        ))->get()->toArray();
+
+        return $users;
+    }
+
+    /*
+    public static function findSuggestedFriends()
+    {
+        $db = DB::table('users')->select('id')->join('relationships', function($join) {
+           $join->on('relationships.user_id', '=', 'users.id');
+        })->join('relationships.follows_user_id', function($join1){
+                $join1->on('relationships.follows_user_id', 'relationships.user_id', '=', 'relationships.user_id', 'relationships.follows_user_id');
+            })->join('users', 'id', '=', 'relationships.follows_user_id');
+
+        print $db->toSql();
+
+        print json_encode($db->get());
+    }
+
+    */
+}
