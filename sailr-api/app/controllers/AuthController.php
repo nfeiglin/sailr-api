c44cfa2610e5e3dd8567d776e983f93954e2f02f
Nathan Feiglin <nathan@technewsu.com.au>
2014-04-05 13:06:31 +1100

    Changes auth code to allow login with username or email

--- sailr-api/app/controllers/AuthController.php
+++ sailr-api/app/controllers/AuthController.php
@@ -14,8 +14,24 @@ class AuthController extends BaseController
         $input = Input::all();
         //$input['password'] = Hash::make($input['password']);
         //User::where('username', '=', 'mz');
-        $a = Auth::attempt($input, true, true);
-        print json_encode($input);
+        //$a = Auth::attempt($input, true, true);
+
+        try {
+            if ($input['username'][0] == '@') $input['username'] = ltrim($input['username'], '@');
+            $user = User::where('username', '=', $input['username'])->orWhere('email', '=', $input['username'])->where('password', '=', Hash::make($input['password']))->firstOrFail(array('id', 'username', 'name'))->toArray();
+            $a = Auth::loginUsingId($user['id']);
+        }
+
+        catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            $res = array(
+                'meta' => array(
+                    'statuscode' => 401,
+                    'message' => 'Username, email, or password is incorrect'
+                )
+            );
+            return Response::json($res, 401);
+        }
+
         if (!$a) {
             $res = array(
                 'meta' => array(
@@ -30,7 +46,9 @@ class AuthController extends BaseController
             'meta' => array(
                 'statuscode' => 200,
                 'message' => 'Logged in successfully'
-            )
+            ),
+
+            'data' => $user
         );
         return Response::json($res);
     }
