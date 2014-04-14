d24a34279296b48959151a3fddb3db82f0e48ec7
Nathan Feiglin <nathan@technewsu.com.au>
2014-04-15 01:04:10 +1000

    Seed changes

--- sailr-api/app/database/seeds/DatabaseSeeder.php
+++ sailr-api/app/database/seeds/DatabaseSeeder.php
@@ -11,9 +11,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Eloquent::unguard();
+        /*
+         * Truncate deletes all the data in the DB. You may want to comment these out!
+         */
+        User::truncate();
+        Relationship::truncate();
+        Item::truncate();
 
-        //$this->call('UsersTableSeeder');
-        // $this->call('RelationshipsTableSeeder');
+        $this->call('UsersTableSeeder');
+        $this->call('RelationshipsTableSeeder');
         $this->call('ItemsTableSeeder');
     }
 
