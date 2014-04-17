5ed17de9af958d97970603b66026051900964c12
Nathan Feiglin <nathan@technewsu.com.au>
2014-04-17 14:42:56 +1000

    Fixes login script

--- sailr-api/app/controllers/AuthController.php
+++ sailr-api/app/controllers/AuthController.php
@@ -16,11 +16,15 @@ class AuthController extends BaseController
         //User::where('username', '=', 'mz');
         //$a = Auth::attempt($input, true, true);
 
+        $a = false;
         try {
             if ($input['username'][0] == '@') $input['username'] = ltrim($input['username'], '@');
-            $user = User::where('username', '=', $input['username'])->orWhere('email', '=', $input['username'])->firstOrFail();
+            $user = User::where('username', '=', $input['username'])->orWhere('email', '=', $input['username'])->with('ProfileImg')->firstOrFail();
             if (Hash::check($input['password'], $user->password)) {
-                $a = Auth::login($user, true)
+                Auth::login($user, true);
+                if(Auth::check()){
+                    $a = true;
+                }
             }
 
         } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
