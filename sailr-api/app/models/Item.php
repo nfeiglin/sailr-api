30b4af44b186544321663f3028115df6dcf5c705
Nathan Feiglin <nathan@technewsu.com.au>
2014-04-19 03:30:49 +1000

    Add comment attribute (default off)

--- sailr-api/app/models/Item.php
+++ sailr-api/app/models/Item.php
@@ -5,6 +5,7 @@ class Item extends Eloquent
     protected $hidden = array('updated_at', 'deleted_at', 'user_id');
     protected $fillable = ['user_id', 'price', 'currency', 'initial_units', 'description', 'title'];
     protected $softDelete = true;
+    //protected $appends = ['comments'];
 
     public function user()
     {
@@ -27,4 +28,10 @@ class Item extends Eloquent
         'currency' => 'required|alpha|min:3|max:3|in:AUD,CAD,EUR,GBP,JPY,USD,NZD,CHF,HKD,SGD,SEK,DKK,PLN,NOK,HUF,CZK,ILS,MXN,PHP,TWD,THB,RUB',
         'initial_units' => 'required|min:1|max:9999999',
     );
+
+    public function getCommentsAttribute() {
+        $comments = Comment::where('item_id', '=', $this->id);
+
+        return $this->attributes['comments'] = $comments;
+    }
 }
