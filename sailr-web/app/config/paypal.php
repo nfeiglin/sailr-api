4f0860f4353b028242961738b4b2d2325a67e8b0
Nathan Feiglin <nathan@technewsu.com.au>
2014-04-21 02:12:52 +1000

    Adds paypal config file and buy controller

---
+++ sailr-web/app/config/paypal.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+
+    'sandbox' => array(
+        'mode' => 'sandbox',
+        'acct1.UserName' => 'nathanfeiglin_api1.gmail.com',
+        'acct1.Password' => 'E4U49983LAWQ9TDE',
+        'acct1.Signature' => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A1aKBONfUE1iGLBP4rqL46IX48rm',
+        'acct1.AppId' => 'APP-80W284485P519543T'
+    ),
+
+    'live' => array(
+        'mode' => 'live',
+        'acct1.UserName' => 'nathanfeiglin_api1.gmail.com',
+        'acct1.Password' => 'E4U49983LAWQ9TDE',
+        'acct1.Signature' => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A1aKBONfUE1iGLBP4rqL46IX48rm',
+        'acct1.AppId' => 'APP-80W284485P519543T'
+    )
+
+
+
+);
---
+++ sailr-web/app/controllers/BuyController.php
@@ -0,0 +1,65 @@
+<?php
+
+class BuyController extends \BaseController {
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 * GET /buy/{id}/create
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function create($id)
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /buy
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 * GET /buy/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 * GET /buy/{id}/edit
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 * PUT /buy/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+}
