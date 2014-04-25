0535ff617ee716d2941954a990ead211fc25dc4e
Nathan Feiglin <nathan@technewsu.com.au>
2014-04-25 14:13:24 +1000

    Fixes stupid copy and paste oversight where login view would post to User create and button said "sign up"

--- sailr-web/app/views/session/create.blade.php
+++ sailr-web/app/views/session/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <link rel="stylesheet" href="{{ URL::asset('css/login.css') }}">
 
-<form class="form-signin" role="form" action="{{URL::action('UsersController@store')}}" method="post" validate="validate">
+<form class="form-signin" role="form" action="{{URL::action('SessionController@store')}}" method="post" validate="validate">
     <a href="{{url('')}}"><img src="/images/logo-500.png" class="img-responsive"></a>
     @if(Session::get('message'))
     	<p class="form-signin message h5 well well-sm text-danger">{{ Session::get('message') }}</p>
@@ -13,6 +13,6 @@
     <input type="text" class="form-control" value="{{{ Input::old('username') }}}" placeholder="Email address or Username" name="username" required="required" autofocus>
     <input type="password" class="form-control" placeholder="Password" name="password" required="required">
 
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
 </form>
 @stop
