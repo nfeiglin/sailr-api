4ae1ad694c7085a512b534773ef7444811dc966c
Nathan Feiglin <nathan@technewsu.com.au>
2014-05-03 18:59:53 +1000

    Adds checkout model to keep track of paypal purchases /requests

---
+++ migrations/2014_05_03_081637_create_checkouts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCheckoutsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('checkouts', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->unsignedInteger('item_id');
+			$table->unsignedInteger('user_id');
+			$table->string('token', 30)->nullable();
+			$table->string('ack', 30)->nullable();
+			$table->boolean('completed');
+			$table->timestamps();
+            $table->softDeletes();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('checkouts');
+	}
+
+}
