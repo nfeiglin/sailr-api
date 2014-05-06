c06bd924206a5809beeedf6f384c51d87af85215
Nathan Feiglin <nathan@technewsu.com.au>
2014-05-06 18:53:21 +1000

    Makes sandbox paypal work with updated SANBOX only credentials

--- sailr-web/app/config/paypal.php
+++ sailr-web/app/config/paypal.php
@@ -2,10 +2,10 @@
 return array(
 
     'sandbox' => array(
-        'mode' => 'SANDBOX',
-        'acct1.UserName' => 'nathanfeiglin_api1.gmail.com',
-        'acct1.Password' => 'E4U49983LAWQ9TDE',
-        'acct1.Signature' => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A1aKBONfUE1iGLBP4rqL46IX48rm',
+        'mode' => 'sandbox',
+        'acct1.UserName' => 'nathanfeiglin-facilitator_api1.gmail.com',
+        'acct1.Password' => '1398008368',
+        'acct1.Signature' => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A4.g5nKX5kOhyfxayMhcYtgXSaMT',
         'acct1.AppId' => 'APP-80W284485P519543T'
     ),
 
