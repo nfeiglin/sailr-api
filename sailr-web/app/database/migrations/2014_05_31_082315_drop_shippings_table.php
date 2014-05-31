a70409e142869e3d568550f69edb9e6c32b1665a
Nathan Feiglin <nathan@technewsu.com.au>
2014-05-31 18:25:43 +1000

    Add migration to get rid of unneeded shipping info table

---
+++ sailr-web/app/database/migrations/2014_05_31_082315_drop_shippings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class DropShippingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::drop('shippings');
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::create('shippings', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->unsignedInteger('item_id');
+			$table->string('type', 40);
+			$table->decimal('price', 5, 2);
+            $table->string('desc', 400);
+			$table->timestamps();
+            $table->softDeletes();
+		});
+	}
+
+}
