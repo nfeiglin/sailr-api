5f034b91a8e37e174a6fdff9e4674c8e79c5d030
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-01 01:34:20 +1000

    Uncomments the seeders

--- sailr-web/app/database/seeds/DatabaseSeeder.php
+++ sailr-web/app/database/seeds/DatabaseSeeder.php
@@ -15,18 +15,16 @@ class DatabaseSeeder extends Seeder
 
         /*
          * Truncate deletes all the data in the DB. You may want to comment these out!
-         */
-        /*
-
-        Item::truncate();
         */
-
-        //User::truncate();
-        //Relationship::truncate();
-        //$this->call('UsersTableSeeder');
-        //$this->call('RelationshipsTableSeeder');
+        
+        Item::truncate();
+        User::truncate();
+        Relationship::truncate();
+        
+        $this->call('UsersTableSeeder');
+        $this->call('RelationshipsTableSeeder');
         $this->call('ItemsTableSeeder');
-        //$this->call('ProfileImgsTableSeeder');
+        $this->call('ProfileImgsTableSeeder');
     }
 
 }
