21328351a855834972b0711e16e35f572a115ec8
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-01 01:34:03 +1000

    Remove country col from items migration

---
+++ sailr-web/app/database/migrations/2014_05_31_143639_remove_country_from_items.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class RemoveCountryFromItems extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('items', function(Blueprint $table)
+		{
+			$table->dropColumn('country');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('items', function(Blueprint $table)
+		{
+			$table->string('country');
+		});
+	}
+
+}
