174e8b04baca9432c9e255cdb72adb51c46b0574
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-08 01:06:37 +1000

    Adds notification.show code

--- sailr-web/app/views/notifications/show.blade.php
+++ sailr-web/app/views/notifications/show.blade.php
@@ -1,1 +1,33 @@
-/home/vagrant/web/app/views/notifications/show.blade.php
+@extends('layout.main')
+
+@section('head')
+<script>
+    var sailr = {
+        notifications: {{ $notifications }}
+    };
+</script>
+<script src="{{ URL::asset('js/controllers/notifications/notificationsController.js') }}"></script>
+@stop
+
+@section('content')
+
+<div data-ng-controller="notificationsController">
+
+    <div class="row">
+        <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+            <div class="panel" ng-repeat="notification in notifications">
+                <div class="panel-body">
+                    <div class="long" ng-if="long_html in notification">
+                        @{{ notification.long_html }}
+                    </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+</div>
+
+
+@stop
