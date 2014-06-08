00e462e9fb6195245641298ba0691ff670869349
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-08 18:28:10 +1000

    Removes unused methods

--- sailr-web/app/controllers/BuyController.php
+++ sailr-web/app/controllers/BuyController.php
@@ -493,41 +493,6 @@ class BuyController extends \BaseController
         //dd($input);
     }
 
-    /**
-     * Display the specified resource.
-     * GET /buy/{id}
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * GET /buy/{id}/edit
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * PUT /buy/{id}
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        //
-    }
 
 
 }
