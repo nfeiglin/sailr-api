1283cb65aa59d613d3575c660bcd5647fb84809c
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-09 16:39:58 +1000

    Adds relationships to model

--- sailr-web/app/models/Checkout.php
+++ sailr-web/app/models/Checkout.php
@@ -5,6 +5,16 @@ use Illuminate\Database\Eloquent\SoftDeletingTrait;
 class Checkout extends \Eloquent {
     use SoftDeletingTrait;
     protected $dates = ['created_at', 'updated_at'];
-
+    protected $hidden = [];
 	protected $fillable = [];
+
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
+
+    public function item()
+    {
+        return $this->belongsTo('Item');
+    }
 }
