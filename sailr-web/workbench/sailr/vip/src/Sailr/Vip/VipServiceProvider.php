81d18b59ec002967c0f6b71103025ff2b929726c
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-12 22:50:20 +1000

    Adds VIP package

---
+++ sailr-web/workbench/sailr/vip/src/Sailr/Vip/VipServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Sailr\Vip;
+
+use Illuminate\Support\ServiceProvider;
+
+class VipServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('sailr/vip');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
