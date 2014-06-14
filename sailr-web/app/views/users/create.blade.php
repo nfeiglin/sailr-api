b36ed8d7c209ee0aff8ab7c430474324f4ce19fb
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-15 03:26:30 +1000

    Begins to add buttons for user to choose plan on signup

--- sailr-web/app/views/users/create.blade.php
+++ sailr-web/app/views/users/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<form class="form-signin register panel" role="form" action="{{URL::action('UsersController@store')}}" method="post" validate="validate">
+<form class="form-signin register panel wide" role="form" action="{{URL::action('UsersController@store')}}" method="post" validate="validate">
     {{ Form::token() }}
     <a href="{{url('')}}"><img src="/images/logo-500.png" class="img-responsive"></a>
     @if(Session::get('message'))
@@ -18,6 +18,31 @@
     <input type="text" class="form-control" value="{{{ Input::old('username') }}}" placeholder="Username" name="username" required="required">
     <input type="password" class="form-control" placeholder="Password" name="password" required="required">
 
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
+    <div class="plan-chooser">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="paid col-lg-8 col-md-8 col-sm-6 col-xs-12">
+                <div class="square-btn">
+                    <div class="square-btn-content">
+                        <span class="glyphicon glyphicon-tower big-font text-center"></span>
+                        <div class="h3">Awesome</div>
+                        <ul>
+                            <li>A point</li>
+                            <li>Goes here!</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="free col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="square-btn">
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <button class="btn btn-lg btn-turq btn-block btn-big" type="submit">Sign up</button>
 </form>
 @stop
