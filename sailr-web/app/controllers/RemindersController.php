e05e07ffd24439ccce84112228aa87d5014abb11
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-17 21:35:12 +1000

    Adds subject to password reminder email

--- sailr-web/app/controllers/RemindersController.php
+++ sailr-web/app/controllers/RemindersController.php
@@ -19,7 +19,12 @@ class RemindersController extends Controller {
 	 */
 	public function postRemind()
 	{
-		switch ($response = Password::remind(Input::only('email')))
+        $response = Password::remind(Input::only('email'), function($message) {
+
+            $message->subject("[Sailr] Reset your password");
+        });
+
+		switch ($response)
 		{
 			case Password::INVALID_USER:
 				return Redirect::back()->with('message', Lang::get($response));
