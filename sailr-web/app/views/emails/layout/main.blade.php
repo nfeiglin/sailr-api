0f9d5845cecdbf2fd7a42d2a9e7bf82e50831712
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-22 01:44:09 +1000

    Adds conditional to allow for 'partial' rendering with no whole <html> DOM + style tag setup

--- sailr-web/app/views/emails/layout/main.blade.php
+++ sailr-web/app/views/emails/layout/main.blade.php
@@ -1,8 +1,10 @@
+@if(!isset($isPartial))
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width"/>
+
     @include('emails.layout.css.ink')
 
     <style type="text/css">
@@ -11,6 +13,8 @@
 
     </style>
 </head>
+@endif
+
 <body>
 <table class="body">
     <tr>
@@ -23,5 +27,8 @@
         </td>
     </tr>
 </table>
+
+@if(!isset($isPartial))
 </body>
 </html>
+@endif
