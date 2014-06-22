9ea21b4cb6f6e592dd6f7e4efcbe357424cdce73
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-23 00:24:26 +1000

    Adds more conditionals to handle partial rendering of emails (minus CSS and HTML tag)

--- sailr-web/app/views/emails/layout/main.blade.php
+++ sailr-web/app/views/emails/layout/main.blade.php
@@ -1,4 +1,5 @@
-@if(!isset($isPartial))
+@if(isset($isPartial))
+@if($isPartial == 1))
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -14,7 +15,7 @@
     </style>
 </head>
 @endif
-
+@endif
 <body>
 <table class="body">
     <tr>
@@ -28,7 +29,9 @@
     </tr>
 </table>
 
-@if(!isset($isPartial))
+@if(isset($isPartial))
+@if($isPartial == 1)
 </body>
 </html>
+@endif
 @endif
