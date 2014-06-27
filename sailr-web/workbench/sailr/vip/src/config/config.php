d59c69b7c8e7a0df74085fd47733577d8abf1903
Nathan Feiglin <nathan@technewsu.com.au>
2014-06-28 02:18:21 +1000

    Restructures VIP config array so it works!

--- sailr-web/workbench/sailr/vip/src/config/config.php
+++ sailr-web/workbench/sailr/vip/src/config/config.php
@@ -1,24 +1,27 @@
 <?php
 return [
-  'plans' => [
-    'awesome' => [
-        'count' => [
-            'product.create' => [
-                'db' => DB::table('items'),
-                'max' => 40
+    'plans' => [
+        'awesome' => [
+            'count' => [
+                'product' => [
+                    'create' => [
+                        'db' => \DB::table('items'),
+                        'max' => 999999999999999
+                    ]
+                ]
+
+            ]
+        ],
+
+        'default' => [
+            'count' => [
+                'product' => [
+                    'create' => [
+                        'db' => \DB::table('items'),
+                        'max' => 4
+                    ]
+                ]
             ]
         ]
-    ],
-
-    'default' => [
-         'count' => [
-             'product' => [
-                 'create' => [
-                     'db' => DB::table('items'),
-                     'max' => 4
-                 ]
-             ]
-         ]
     ]
-  ]
 ];
