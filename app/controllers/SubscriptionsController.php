fa47f4a19f0517d544807752f1cb21eb7c9a40e0
Nathan Feiglin <nathan@technewsu.com.au>
2014-07-04 13:22:39 +1000

    Adds in significant Stripe error handling on subscription

--- app/controllers/SubscriptionsController.php
+++ app/controllers/SubscriptionsController.php
@@ -39,9 +39,8 @@ class SubscriptionsController extends \BaseController {
         $subscriptionID = 'awesome';
 
 
-        $hasSubscription = $user->subscribed();
 
-        if ($hasSubscription) {
+        if ($user->subscribed()) {
             $res = ['message' => 'You are already subscribed'];
             return Response::json($res, 400);
         }
@@ -51,14 +50,67 @@ class SubscriptionsController extends \BaseController {
         if (Input::has('coupon')) {
             $subscription->withCoupon(Input::get('coupon'));
         }
-        $subscription->create($creditCardToken,
-            ['email'=> $user->email,
-            'metadata' => ['name' => $user->name, 'user_id' => $user->id]
-            ]
-        );
 
-        $res = ['message' => "✓ Subscription successful. You're officially awesome!", 'redirect_url' => URL::to('/')];
-        return Response::json($res, 201);
+
+        try {
+
+            $subscription->create($creditCardToken,
+                ['email' => $user->email,
+                    'metadata' => ['name' => $user->name, 'username' => $user->username, 'user_id' => $user->id]
+                ]);
+
+            $res = ['message' => "✓ Subscription successful. You're officially awesome!", 'redirect_url' => URL::to('/')];
+            return Response::json($res, 201);
+
+        } catch (Stripe_CardError $e) {
+            // Since it's a decline, Stripe_CardError will be caught
+            $body = $e->getJsonBody();
+            $err = $body['error'];
+
+            $res = ['message' => $err['message']];
+            return Response::json($res, 400);
+
+        } catch (Stripe_InvalidRequestError $e) {
+            // Invalid parameters were supplied to Stripe's API
+            $body = $e->getJsonBody();
+            $err = $body['error'];
+
+            $res = ['message' => $err['message']];
+            return Response::json($res, 400);
+
+        } catch (Stripe_AuthenticationError $e) {
+            // Authentication with Stripe's API failed
+            // (maybe you changed API keys recently)
+
+            $body = $e->getJsonBody();
+            $err = $body['error'];
+
+            $res = ['message' => $err['message']];
+            return Response::json($res, 400);
+
+        } catch (Stripe_ApiConnectionError $e) {
+            // Network communication with Stripe failed
+            $body = $e->getJsonBody();
+            $err = $body['error'];
+
+            $res = ['message' => $err['message']];
+            return Response::json($res, 400);
+        } catch (Stripe_Error $e) {
+            // Display a very generic error to the user, and maybe send
+            // yourself an email
+            Log::error('Stripe error:: ' . print_r($e->getJsonBody(), 1));
+
+            $body = $e->getJsonBody();
+            $err = $body['error'];
+            $res = ['message' => $err['message']];
+            return Response::json($res, 400);
+        } catch (Exception $e) {
+            // Something else happened, completely unrelated to Stripe
+            Log::error($e->getMessage() . $e->getTraceAsString());
+            $res = ['message' => 'Sorry, an error has occurred'];
+            return Response::json($res, 400);
+        }
+
 
 	}
 
