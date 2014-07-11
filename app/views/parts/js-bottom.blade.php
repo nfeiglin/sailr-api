f12a307ee3d0151cb5432330f4559aa9d2c5110c
Nathan Feiglin <nathan@technewsu.com.au>
2014-07-12 04:06:32 +1000

    Regorinises inclusion of assets and comments out testing mass-asset thinggy

--- app/views/parts/js-bottom.blade.php
+++ app/views/parts/js-bottom.blade.php
@@ -1,12 +1,19 @@
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.18/angular.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.18/angular-sanitize.min.js"></script>
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="{{ URL::asset('build/js/main.min.js') }}"></script>
 
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" async="async"></script>
+<script src="{{ URL::asset('build/js/main.js') }}"></script>
+{{-- @if(App::environment('local', 'debug', 'testing'))
+{{ \Sailr\TestPipe\TestPipe::make()->tags('js') }}
+{{ @else }}
+@endif
+--}}
+
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" async="async" defer="defer"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/humane-js/3.0.6/humane.min.js" async="async" defer="defer"></script>
 
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 <script defer="defer" async="async">
     Stripe.setPublishableKey('{{ User::getStripePublishableKey() }}');
 </script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/humane-js/3.0.6/themes/original.min.css">
+
--- app/views/parts/js-head.blade.php
+++ app/views/parts/js-head.blade.php
@@ -1,7 +1,15 @@
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <link href='//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="{{ URL::asset('build/css/all.min.css') }}">
-<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.18/angular.min.js"></script>
+
+{{--
+@if(App::environment('local', 'debug', 'testing'))
+{{ \Sailr\TestPipe\TestPipe::make()->tags('css') }}
+@else
+--}}
+<link rel="stylesheet" href="{{ URL::asset('build/css/base.min.css') }}">
+{{-- @endif --}}
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/humane-js/3.0.6/themes/original.min.css">
 
 <script>
     var csrfToken = '{{ Session::token() }}';
