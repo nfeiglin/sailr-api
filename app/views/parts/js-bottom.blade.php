8e1ff8bd4d5a7326fb405a306ed3246b3f4e474d
Nathan Feiglin <nathan@technewsu.com.au>
2014-07-12 19:58:39 +1000

    Repositions JS and CSS

--- app/views/parts/js-bottom.blade.php
+++ app/views/parts/js-bottom.blade.php
@@ -1,19 +1,13 @@
-<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.18/angular.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.18/angular-sanitize.min.js"></script>
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.19/angular.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.19/angular-sanitize.min.js" defer="defer"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.3.6/slick.min.js" async="async"></script>
 <script src="{{ URL::asset('build/js/main.js') }}"></script>
-{{-- @if(App::environment('local', 'debug', 'testing'))
-{{ \Sailr\TestPipe\TestPipe::make()->tags('js') }}
-{{ @else }}
-@endif
---}}
 
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" async="async" defer="defer"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/humane-js/3.0.6/humane.min.js" async="async" defer="defer"></script>
 
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-<script defer="defer" async="async">
+<script async="async">
     Stripe.setPublishableKey('{{ User::getStripePublishableKey() }}');
 </script>
-
