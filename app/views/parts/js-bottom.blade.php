e711a64670d1a7568cd70045bebd0c964cdb0715
Nathan Feiglin <nathan@technewsu.com.au>
2014-07-12 20:40:18 +1000

    Calls the main.min.js rather than without the min.. ;)

--- app/views/parts/js-bottom.blade.php
+++ app/views/parts/js-bottom.blade.php
@@ -2,8 +2,7 @@
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.19/angular-sanitize.min.js" defer="defer"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.3.6/slick.min.js" async="async"></script>
-<script src="{{ URL::asset('build/js/main.js') }}"></script>
-
+<script src="{{ URL::asset('build/js/main.min.js') }}"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" async="async" defer="defer"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/humane-js/3.0.6/humane.min.js" async="async" defer="defer"></script>
 
