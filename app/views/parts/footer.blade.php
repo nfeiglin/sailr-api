bd8fae9ff8161883d491bbd07a66911cc94d8050
Nathan Feiglin <nathan@technewsu.com.au>
2014-07-14 16:20:36 +1000

    Makes footer conditionally show login / signup button or logout button depending on if user is logged in or not

--- app/views/parts/footer.blade.php
+++ app/views/parts/footer.blade.php
@@ -1,7 +1,10 @@
     <div class="page-footer" id="footer" sailr-footer>
         <div class="container">
             <p class="centered">
-                <a href="{{ URL::to('/') }}">Home</a> |  <a href="{{ URL::action('contact') }}">Contact us</a> | <a href="{{ URL::action('UsersController@create') }}">Sign up</a> | <a href="{{ URL::action('SessionController@create') }}">Login</a>
+                <a href="{{ URL::to('/') }}">Home</a> |  <a href="{{ URL::action('contact') }}">Contact us</a> |
+                @if(!Auth::check())
+                <a href="{{ URL::action('UsersController@create') }}">Sign up</a> | <a href="{{ URL::action('SessionController@create') }}">Login</a>
+                @else<a href="{{ URL::action('SessionController@destroy') }}">Logout</a>@endif
             </p>
 
             <p class="terms centered">
