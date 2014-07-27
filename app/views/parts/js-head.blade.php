4946d1eb5d9be6d8ff36ee092f3ff82c36eb5e90
Nathan Feiglin <nathan@technewsu.com.au>
2014-07-27 19:06:23 +1000

    Changes JS and CSS urls to /v1/ to reload them

--- app/views/parts/js-head.blade.php
+++ app/views/parts/js-head.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700|Lato:400,700' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="{{ URL::asset('build/css/base.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('build/v1/css/base.min.css') }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/humane-js/3.0.6/themes/original.min.css">
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.3.6/slick.css"/>
 <script>
