b328663e22839f9fafe599ec3f24c99312316c53
Nathan Feiglin <nathan@technewsu.com.au>
2014-08-31 03:31:15 +1000

    Adds base repository and some methods

---
+++ app/Sailr/Repositories/BaseRepository.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nathan
+ * Date: 31/08/2014
+ * Time: 3:17 AM
+ */
+
+namespace Sailr\Repository;
+
+class BaseRepository {
+
+    /**
+     * Class BaseRepository
+     * @package Sailr\Repository
+     */
+
+    /**
+     * @var \Model
+     */
+    protected $model;
+
+    public function ___construct(\Model $model) {
+        $this->model = $model;
+    }
+
+    /**
+     * Make a new instance of the entity to query on
+     *
+     * @param array $with
+     */
+    public function make(array $with = [])
+    {
+        return $this->model->with($with);
+    }
+
+    /**
+     * Find an entity by id
+     *
+     * @param int $id
+     * @return Illuminate\Database\Eloquent\Model
+     */
+    public function getById($id)
+    {
+        return $this->model->find($id);
+    }
+
+    /**
+     * Find a single entity by key value
+     *
+     * @param string $key
+     * @param string $value
+     * @param array $with
+     */
+    public function getFirstBy($key, $value, array $with = array())
+    {
+        $this->make($with)->where($key, '=', $value)->first();
+    }
+
+    /**
+     * Find many entities by key value
+     *
+     * @param string $key
+     * @param string $value
+     * @param array $with
+     */
+    public function getManyBy($key, $value, array $with = array())
+    {
+        $this->make($with)->where($key, '=', $value)->get();
+    }
+
+    /**
+     * Return all results that have a required relationship
+     *
+     * @param string $relation
+     */
+    public function has($relation, array $with = array())
+    {
+        $entity = $this->make($with);
+
+        return $entity->has($relation)->get();
+    }
+
+    /**
+     * Get Results by Page
+     *
+     * @param int $page
+     * @param int $limit
+     * @param array $with
+     * @return \StdClass Object with $items and $totalItems for pagination
+     */
+    public function getByPage($page = 1, $limit = 10, $with = array())
+    {
+        //Based off http://culttt.com/2014/03/17/eloquent-tricks-better-repositories/
+
+        $result = new \StdClass;
+        $result->page = $page;
+        $result->limit = $limit;
+        $result->totalItems = 0;
+        $result->items = array();
+
+        $query = $this->make($with);
+
+        $model = $query->skip($limit * ($page - 1))
+            ->take($limit)
+            ->get();
+
+        $result->totalItems = $this->model->count();
+        $result->items = $model->all();
+
+        return $result;
+    }
+
+} 
