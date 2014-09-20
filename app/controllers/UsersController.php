eb7f6e1afc5422c0968cade526877a24eb29214f
Nathan Feiglin <nathan@technewsu.com.au>
2014-09-21 00:30:22 +1000

    Separates UsersController@show and the user's recent activity feed

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -88,72 +88,9 @@ class UsersController extends \BaseController
     public function show($username)
     {
 
-        //How many results per page?
-        $resultsPerPage = 30;
-
         $user = $this->repository->getFirstOrFailBy('username', $username, ['id', 'name', 'username', 'bio'], ['ProfileImg']);
-        //dd($user->toJson());
+        return $this->responder->showSingleModel($user);
 
-        $items = $user->items()->where('public', '=', 1)->with(['User', 'Photos' => function($y) {
-          $y->where('type', '=', 'full_res');
-          $y->select(['url', 'id', 'item_id']);
-        }]);
-
-        $feedCollectionBuilder = new \Sailr\ApiFeed\FeedCollectionBuilder;
-        $feedCollectionBuilder->createItemsFeed($items->get());
-
-        return Response::json($feedCollectionBuilder->getFeedCollection());
-
-        //return Response::json($items->get());
-
-
-        $paginator = $items->paginate($resultsPerPage);
-        $items = $items->get();
-        $items = $items->toArray();
-
-
-
-
-        $isSelf = false;
-        $follow_you = false;
-        $you_follow = false;
-        if (Auth::check()) {
-            if (Auth::user()->username == $username) {
-                $isSelf = true;
-            }
-
-            $follow_you = RelationshipHelpers::follows_you($user);
-            $you_follow = RelationshipHelpers::you_follow($user);
-        }
-
-
-        if (Auth::check()) {
-
-        }
-
-        $no_of_followers = RelationshipHelpers::count_follows_user($user);
-        $no_of_following = RelationshipHelpers::count_user_following($user);
-
-        $mutual = false;
-
-        if ($follow_you && $you_follow) {
-            $mutual = true;
-        }
-
-        $userArray = $user->toArray();
-
-        return View::make('users.show')
-        ->with('title', $user['username'])
-        ->with('user', $userArray)
-        ->with('items', $items)
-        ->with('paginator', $paginator)
-        ->with('follows_you', $follow_you)
-        ->with('you_follow', $you_follow)
-        ->with('mutual', $mutual)
-        ->with('is_self', $isSelf)
-        ->with('no_of_followers', $no_of_followers)
-        ->with('no_of_following', $no_of_following)
-        ;
     }
 
     /**
