9ea2fd5560483464426d888062ed45d49fa2a0a2
Nathan Feiglin <nathan@technewsu.com.au>
2014-09-22 00:55:23 +1000

    Cleans up the routes TODO: Add route filters

--- app/routes.php
+++ app/routes.php
@@ -2,10 +2,12 @@
 
 
 
-
+/* Webhook Routes === THESE ARE IMPORTANT !!! */
 Route::post('purchase/{id}', 'BuyController@store', ['before' => 'auth']);
 Route::post('payment/ipn', array('uses' => 'IpnController@store', 'as' => 'ipn'));
 Route::post('payment/stripe-webhook', 'Laravel\Cashier\WebhookController@handleWebhook');
+
+/* KEEP THE WEBHOOK ROUTES */
 
 /*
 |--------------------------------------------------------------------------
@@ -18,99 +20,63 @@ Route::post('payment/stripe-webhook', 'Laravel\Cashier\WebhookController@handleW
 |
 */
 
-/*
-Event::listen('illuminate.query', function($sql, $bindings, $time){
-    //echo $sql;          // select * from my_table where id=? 
-    //print_r($bindings); // Array ( [0] => 4 )
-    echo $time;         // 0.58 
-
-    // To get the full sql query with bindings inserted
-    $sql = str_replace(array('%', '?'), array('%%', '%s'), $sql);
-    $full_sql = vsprintf($sql, $bindings);
-    echo '<pre>' . $full_sql . '</pre>';
-});
-*/
-
-/*
-Route::bind('user', function($value, $route)
-{
-    $repo = new Sailr\Repository\UsersRepository(new \User);
-    $repo->make(['ProfileImg']);
-
-    return $repo->getFirstOrFailBy('id', $value);
-});
-*/
-
 Route::get('/', function(){
     return Response::json('API Base URL');
 });
 
+/* User Routes */
 Route::resource('users', 'UsersController');
-Route::get('users/{user}', 'UsersController@show');
+Route::get('users/{id}/feed', 'FeedsController@show');
+Route::get('users/{id}/following', 'UsersController@following');
+Route::get('users/{id}/followers', 'UsersController@followers');
 
+/* Collection Routes */
+Route::resource('users/{id}/collections', 'CollectionsController');
+Route::post('users/{id}/collections/favourite', 'CollectionsController@favourite');
+Route::delete('collections/destroy/{collection_id}', 'CollectionsController@destroyCollection');
+Route::delete('collections/destroy/{collection_id}/item/{item_id}', 'CollectionsController@destroyCollectionItem');
+
+/* Items Routes */
+Route::resource('items', 'ItemsController', ['only' => ['store', 'show', 'edit', 'update', 'index']]);
+Route::put('items/{id}/toggle-visibility', 'ItemsController@toggleVisibility');
+Route::post('items/{id}/toggle-visibility', 'ItemsController@toggleVisibility');
+
+/* Photos Routes */
+Route::post('photos/{item_id}', 'PhotosController@store');
+Route::delete('photos/{item_id}/{set_id?}', 'PhotosController@destroy');
+
+/* Profile Image Routes */
+Route::resource('users/profile-image', 'ProfileImageController'); // (For the logged in user)
+
+/* Comments Routes */
+Route::resource('comments', 'CommentsController', ['only' => ['create', 'store', 'show', 'destroy']]);
+Route::get('items/{items_id}/comments', 'CommentsController@item_comments');
+
+/* Relationship Routes */
+Route::resource('relationship', 'RelationshipsController');
+
+/* Feeds Routes */
+Route::resource('feeds', 'FeedsController');
+
+/* Searches Routes */
 Route::get('/searches/{query}', 'SearchesController@show');
-Route::get('{username}/following', 'UsersController@following');
-Route::get('{username}/followers', 'UsersController@followers');
+
+/* Notification Routes */
+Route::resource('notifications', 'NotificationsController'); //This will likely get integrated into some sort of Feed
+
+/* Sessions Routes */
+Route::resource('sessions', 'SessionController', ['only' => ['create', 'store', 'destroy']]);
+Route::get('login', 'SessionController@create');
+Route::get('logout', 'SessionController@destroy');
+Route::get('signup', 'UsersController@create');
 
 
-    Route::resource('items', 'ItemsController', ['only' => ['store', 'show', 'edit', 'update', 'index']]);
-
-    Route::group(['before' => 'guest'], function () {
-        Route::resource('session', 'SessionController', ['only' => ['create', 'store']]);
-        Route::resource('user/create', 'UsersController@create');
-    });
-    Route::get('session/destroy', 'SessionController@destroy');
-    Route::controller('password', 'RemindersController');
-
-
-    Route::get('/{username}/product/{id}', 'BuyController@create');
-    
-    Route::resource('{username}/collections', 'CollectionsController');
-    Route::get('{username}/collections', 'CollectionsController@index');
-    Route::get('{username}/collections/{id}', 'CollectionsController@show');
-    Route::get('api/collections/{username}/all', 'CollectionsController@index');
-
-
-
-    Route::group(['before' => ['auth']], function () {
-
-        Route::resource('settings/user/profile-image', 'ProfileImageController');
-        Route::post('photo/upload/{item_id}', 'PhotosController@store');
-        Route::delete('photo/{item_id}', 'PhotosController@destroy');
-        Route::put('photo/{item_id}', 'PhotosController@destroy');
-
-
-        Route::resource('relationship', 'RelationshipsController');
-
-        Route::get('/{username}/product/{id}/comments', 'CommentsController@item_comments');
-        Route::resource('comments', 'CommentsController', ['only' => ['create', 'store', 'show', 'destroy']]);
-
-
-        Route::put('products/{id}/toggle-visibility', 'ItemsController@toggleVisibility');
-        Route::post('products/{id}/toggle-visibility', 'ItemsController@toggleVisibility');
-        Route::resource('notifications', 'NotificationsController');
-
-        Route::post('collections/favourite', 'CollectionsController@favourite');
-        Route::post('collections/store', 'CollectionsController@store');
-        Route::delete('collections/destroy/{id}', 'CollectionsController@destroyCollection');
-        Route::delete('collections/destroy/{collection_id}/item/{item_id}', 'CollectionsController@destroyCollectionItem');
-    });
-
-
-    Route::group(array('prefix' => 'self'), function () {
-        Route::get('login', 'SessionController@create');
-        Route::get('logout', 'SessionController@destroy');
-        Route::get('signup', 'UsersController@create');
-
-        Route::resource('user', 'UsersController', ['only' => ['store', 'show']]);
-
-    });
-
-    Route::group(['prefix' => 'onboard'], function(){
-       Route::group(['prefix' => 'recent'], function() {
-          Route::get('products/{offset?}/{limit?}', 'OnboardController@getRecentProducts');
-       });
-    });
+/* MISC */
+Route::group(['prefix' => 'onboard'], function(){
+   Route::group(['prefix' => 'recent'], function() {
+      Route::get('products/{offset?}/{limit?}', 'OnboardController@getRecentProducts');
+   });
+});
 
 
 
